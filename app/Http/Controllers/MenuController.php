68c3b735959b8442aae30ef2ea2048973c7cd907
Madasamy <madasamy838@gmail.com>
2020-10-16 12:28:16 +0530

    app folder

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+use App\Model\Invoice;
+use App\Model\Estimation;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class MenuController extends Controller {
+	function index(Request $request) { 
+		$amtinsert = Invoice::amtdet(); 
+		$insert = Invoice::amtnewtbl($amtinsert);
+		$estinsert = Estimation::estworkdet(); 
+		$estworkinsert = Estimation::estnewtbl($estinsert);
+		return view('Menu.index',['request'=> $request]);
+	}
+}
+
+?>
