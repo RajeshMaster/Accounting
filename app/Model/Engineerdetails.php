c371e72537d325fef5beb77582efffbc254fedd6
Madasamy <madasamy838@gmail.com>
2020-10-16 13:51:03 +0530

    Remove Salary Calc and Salay plus and Engineerdetails

--- app/Model/Engineerdetails.php
+++ app/Model/Engineerdetails.php
@@ -93,40 +93,6 @@ OR emp_ID10 != '' OR emp_ID11 != '' OR emp_ID12 != '' OR emp_ID13 != '' OR emp_I
       ->get();
     return $result;
   }
-  public static function fngetexpensedetail($year,$month,$request,$flg) {
-    if ($request->historypage == 1) {
-      $yearmonth = "";
-      $empid1 = " AND main.emp_ID = '$request->empid'";
-      $empid2 = " AND pettycash.emp_ID = '$request->empid'";
-    } else {
-      $empid1 = "";
-      $empid2 = "";
-      $yearmonth = $year.'-'.$month;
-    }
-    $result = DB::TABLE(DB::raw("(SELECT main.date,main.emp_ID,emp.Firstname,emp.LastName,main.subject,main.details,sub.Subject as subname,sub.Subject_jp as subjp,
-    subdet.sub_eng,subdet.sub_jap,main.amount,NULL as charge,main.remark_dtl,main.file_dtl
-    FROM dev_expenses as main
-    LEFT JOIN dev_expensesetting sub on sub.id=main.subject
-    LEFT JOIN inv_set_expensesub subdet on subdet.id=main.details AND subdet.mainid=main.subject
-    LEFT JOIN emp_mstemployees emp ON emp.Emp_ID=main.emp_ID
-     WHERE date LIKE '%$yearmonth%' AND main.emp_ID != '' $empid1 
-    UNION ALL
-    SELECT pettycash.date,pettycash.emp_ID,emp.Firstname,emp.LastName,main_subject as subject,pettycash.sub_subject as details,sub.main_eng as subname,
-    sub.main_jap as subjp,subdet.sub_eng,subdet.sub_jap,pettycash.amount,NULL as charge,pettycash.remark_dtl,pettycash.file_dtl  
-    FROM inv_pettycash as pettycash
-    LEFT JOIN inv_set_transfermain sub on sub.id=pettycash.main_subject
-    LEFT JOIN inv_set_transfersub subdet on subdet.id=pettycash.sub_subject AND subdet.mainid=pettycash.main_subject
-    LEFT JOIN emp_mstemployees emp ON emp.Emp_ID=pettycash.emp_ID
-     WHERE date LIKE '%$yearmonth%' AND pettycash.emp_ID != '' $empid2) as DDD"));
-    if ($flg == 1) {
-      $result = $result->get();
-    } else {
-      $result = $result->orderBy($request->engineerdetailssort, $request->sortOrder);
-      $result = $result->paginate($request->plimit);
-    }
-    // ->toSql();dd($result);
-    return $result;
-  }
   /*UNION ALL
     SELECT main.bankdate as date,main.emp_ID,emp.Firstname,emp.LastName,main.subject,main.details,sub.Subject as subname,sub.Subject_jp as subjp,
     subdet.sub_eng,subdet.sub_jap,main.amount,main.fee as charge,main.remark_dtl,main.file_dtl 
@@ -135,49 +101,5 @@ OR emp_ID10 != '' OR emp_ID11 != '' OR emp_ID12 != '' OR emp_ID13 != '' OR emp_I
     LEFT JOIN inv_set_expensesub subdet on subdet.id=main.details AND subdet.mainid=main.subject
     LEFT JOIN emp_mstemployees emp ON emp.Emp_ID=main.emp_ID
      WHERE bankdate LIKE '%$yearmonth%' AND main.emp_ID != ''*/
-  public static function fnGetExpenseRecord($request,$from_date, $to_date) {
-    // ACCESS RIGHTS
-    // CONTRACT EMPLOYEE
-    $conditionAppend = "";
-    if (Auth::user()->userclassification == 1) {
-      $from_date = Auth::user()->accessDate;
-      $conditionAppend = "OR accessFlg = 1";
-    }
-    // END ACCESS RIGHTS
-    $sql = "SELECT SUBSTRING(date, 1, 7) AS date FROM dev_expenses
-            WHERE (date > '$from_date' AND date < '$to_date') AND emp_ID != '' $conditionAppend UNION ALL 
-            /*SELECT SUBSTRING(bankdate, 1, 7) AS date FROM dev_banktransfer 
-            WHERE (bankdate > '$from_date' AND bankdate < '$to_date') AND emp_ID != '' $conditionAppend UNION ALL*/
-            SELECT SUBSTRING(date, 1, 7) AS date FROM inv_pettycash 
-            WHERE (date > '$from_date' AND date < '$to_date') AND emp_ID != '' $conditionAppend ORDER BY date ASC";
-    $query = DB::select($sql);
-    return $query;
-  }
-  public static function fnGetExpenseRecordPrevious($request,$from_date) {
-    // ACCESS RIGHTS
-    // CONTRACT EMPLOYEE
-    $conditionAppend = "";
-    if (Auth::user()->userclassification == 1) {
-      $from_date = Auth::user()->accessDate;
-      $conditionAppend = "OR accessFlg = 1";
-    }
-    // END ACCESS RIGHTS
-    $sql = "SELECT SUBSTRING(date, 1, 7) AS date FROM dev_expenses
-            WHERE (date <= '$from_date' ) AND emp_ID != '' $conditionAppend UNION ALL 
-            /*SELECT SUBSTRING(bankdate, 1, 7) AS date FROM dev_banktransfer
-            WHERE (date <= '$from_date' ) AND emp_ID != '' $conditionAppend UNION ALL*/
-            SELECT SUBSTRING(date, 1, 7) AS date FROM inv_pettycash 
-            WHERE (date <= '$from_date' ) AND emp_ID != '' $conditionAppend ORDER BY date ASC";
-    $query = DB::select($sql);
-    return $query;
-  }
-  public static function fnGetExpenseRecordNext($request,$to_date) {
-    $sql = "SELECT SUBSTRING(date, 1, 7) AS date FROM dev_expenses 
-            WHERE (date >= '$to_date') AND emp_ID != '' UNION ALL 
-            SELECT SUBSTRING(date, 1, 7) AS date FROM inv_pettycash 
-            WHERE (date >= '$to_date' ) AND emp_ID != '' ORDER BY date ASC";
-    $cards = DB::select($sql);
-    return $cards;
-  }
 } 	 			
 
