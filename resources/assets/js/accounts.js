1ff5541a561a42baf11b2e245ab9dc9e7b83dbf9
Rajesh <esakkitrajesh2016m@gmail.com>
2020-10-22 15:52:18 +0900

    Validation for popup

--- resources/assets/js/accounts.js
+++ resources/assets/js/accounts.js
@@ -154,6 +154,20 @@ $(document).ready(function() {
 		var hidloan = $("#hidloan").val();
 		var err = 0;
 		document.getElementById("autoDebitContent").value = "";
+
+		$('input[type=checkbox]:not(:checked)').each(function(){
+			var res = $(this).val().split("$"); 
+			if ($('#'+"loanAmt"+res[4]).val() =="" || $('#'+"loanAmt"+res[4]).val() ==0) {
+				$('#'+"loanAmt"+res[4]).attr("style", "background-color: #E88F8F");
+				err = 1;
+				return false;
+			} else {
+				$('#'+"loanAmt"+res[4]).attr("style", "background-color:none");
+			}
+		});
+		if (err) {
+			return false;
+		}
 		var confirmgroup = confirm("Do You Want To Select Loan?");
 		if(confirmgroup) {
 		
@@ -165,34 +179,20 @@ $(document).ready(function() {
 				var res = $(this).val().split("$"); 
 				if (getchecked == 1) {
 					getchecked = 2;
-					if ($('#'+"loanAmt"+res[4]).val() =="" || $('#'+"loanAmt"+res[4]).val() ==0) {
-						$('#'+"loanAmt"+res[4]).attr("style", "background-color: #E88F8F");
-						err = 1;
-						return false;
-					} else {
-						$('#'+"loanAmt"+res[4]).attr("style", "background-color:none");
-					}
+				
 					$('#hidloan').val($('#hidloan').val() + res);
 					$('#loanName').val($('#loanName').val() + res[0]);
 					document.getElementById('autoDebitAmountloan').innerHTML = $('#autoDebitAmountloan').val() + $('#'+"loanAmt"+res[4]).val();
 					document.getElementById('autoDebitFeeloan').innerHTML = $('#autoDebitFeeloan').val() + $('#'+"loanFee"+res[4]).val();
 				} else {
-					if ($('#'+"loanAmt"+res[4]).val() =="" || $('#'+"loanAmt"+res[4]).val() ==0) {
-						$('#'+"loanAmt"+res[4]).attr("style", "background-color: #E88F8F");
-						err = 1;
-						return false;
-					} else {
-						$('#'+"loanAmt"+res[4]).attr("style", "background-color: none");
-					}
+					
 					$('#hidloan').val($('#hidloan').val() + ";" + res);
 					$('#loanName').val($('#loanName').val() + ";" + res[0]);
 					document.getElementById('autoDebitAmountloan').innerHTML = document.getElementById('autoDebitAmountloan').innerHTML + ";" + $('#'+"loanAmt"+res[4]).val();
 					document.getElementById('autoDebitFeeloan').innerHTML = document.getElementById('autoDebitFeeloan').innerHTML + ";" + $('#'+"loanFee"+res[4]).val();
 				}
 			});
-			if (err) {
-				return false;
-			}
+			
 			$("#enableamt").attr("style", "display: block");
 			$("#enablefee").attr("style", "display: block");
 			$("#hidamtfee").attr("style", "display: none");
